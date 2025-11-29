13aaec525a221c0090a7c190e6f1e708b4873640
Hazel Viswanath <viswanath.hazel@gmail.com>
2025-11-29 11:55:56 -0700

    no more ftxui

--- iris.cpp
+++ iris.cpp
@@ -1,27 +1,13 @@
 #include <iostream>
-#include <ftxui/dom/elements.hpp>
-#include <ftxui/screen/screen.hpp>
 
 #define PROJECT_NAME "iris"
 
 int main(int argc, char **argv) {
-    using namespace ftxui;
     if (argc != 1) {
         std::cout << argv[0] << " takes no arguments.\n";
         return 1;
     }
-    std::cout << "This is project " << PROJECT_NAME << ".\n";
-
-    Element document = hbox({
-        text("left") | border,
-	text("middle") | border | flex,
-	text("right") | border,
-    });
-
-
-    auto screen = Screen::Create(Dimension::Full(), Dimension::Fit(document));
-    Render(screen, document);
-    screen.Print();
+    std::cout << "This is project " << PROJECT_NAME << ".\n"; 
 
     return 0;
 }
